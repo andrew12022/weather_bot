a3469838de41bbe5a13d9055f635c8b0f7016cfb
Андрей Елистратов <elistratov2001.2017@yandex.ru>
2023-09-19 17:29:57 +0400

    functions

---
+++ exceptions.py
@@ -0,0 +1,6 @@
+class WeatherAPIError(Exception):
+    """Исключение для ошибок, связанных с запросами к API погоды."""
+
+
+class TelegramAPIError(Exception):
+    """Исключение для ошибок, связанных с API Telegram."""
